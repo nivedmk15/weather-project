957fe82b56c9b0340491c42eabc80fd9532e9503
nived <nivedmk15@gmail.com>
2025-03-28 14:33:22 +0530

    called weather api taken wanted data from response

--- Weatherproject/weatherapp/views.py
+++ Weatherproject/weatherapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import requests
+import datetime
 
-# Create your views here.
+def home(req):
+    city = 'kannur'
+    if req.POST:
+        city = req.POST['city']
+        
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1751f8eea7b6348525f077ba5866d206"
+    
+    PARAMS = {
+        'units' : 'metric'
+    }
+    
+    data = requests.get(url,PARAMS).json()
+    
+    date = datetime.datetime.today()
+    
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+    
+    
+    
+    return render(req,'weather.html')
