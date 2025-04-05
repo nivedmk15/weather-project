3d5a1026ffe649575648de13f530b6810996b01b
nived <nivedmk15@gmail.com>
2025-04-05 11:31:25 +0530

    render the response into the html page

--- Weatherproject/weatherapp/views.py
+++ Weatherproject/weatherapp/views.py
@@ -6,21 +6,38 @@ def home(req):
     city = 'kannur'
     if req.POST:
         city = req.POST['city']
-        
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1751f8eea7b6348525f077ba5866d206"
+    
+    date = datetime.datetime.today()
+    
     
     PARAMS = {
         'units' : 'metric'
     }
     
-    data = requests.get(url,PARAMS).json()
+    try:
+        # created url with city name and api
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1751f8eea7b6348525f077ba5866d206"
+        data = requests.get(url,PARAMS).json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        
+    except:
+        
+        url = f"https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=1751f8eea7b6348525f077ba5866d206"
+        data = requests.get(url,PARAMS).json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        city = 'kochi'
+        
+    context = {
+        'city':city,
+        'description':description,
+        'icon':icon,
+        'temp':temp,
+        'date':date
+    }
     
-    date = datetime.datetime.today()
     
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
-    
-    
-    
-    return render(req,'weather.html')
+    return render(req,'weather.html',context)
